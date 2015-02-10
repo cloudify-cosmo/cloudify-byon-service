72ae820cf3bf50d0c05da29c60bf212680148277
Michał Soczewka <michal.soczewka@codilime.com>
2015-02-10 18:03:15 +0100

    CFY-2054 Added a utility TCP endpoint scanning mechanism

---
+++ utilities/scan_test.py
@@ -0,0 +1,18 @@
+import itertools
+
+from cloudify_hostpool.backend.scan import scan
+
+
+_START_PORT = 3
+_END_PORT = 50000 + _START_PORT
+
+
+if __name__ == '__main__':
+    r = scan(
+        zip(itertools.repeat('localhost'),
+            xrange(_START_PORT, _END_PORT))
+    )
+    print 'scanned: {0}; open: {1}'.format(len(r),
+                                           ', '.join(str(hp[1])
+                                                     for hp, a in r.iteritems()
+                                                     if a))
